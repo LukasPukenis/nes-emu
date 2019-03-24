a463979f7aeaac16e0d8bab240b3802b4094d690
Lukas Pukenis <lukas.pukenis@gmail.com>
2019-03-24 08:50:03 +0200

    structure test files more efficiently

--- tests/nestest/nestest.spec.ts
+++ tests/nestest/nestest.spec.ts
@@ -1,5 +1,5 @@
 const fs = require('mz/fs');
-import { NES } from '../src/nes';
+import { NES } from '../../src/nes';
 
 declare var __dirname: any;
 
@@ -10,10 +10,10 @@ test("nestest.nes rom", async () => {
         let lines = whole.split('\n');
 
         let nes = new NES(null);
-        await nes.load('tests/nestest.nes', 0xC000);
+        await nes.load('tests/nestest/nestest.nes', 0xC000);
 
         // nes.getCPU().step();
-        for (let line of lines) {
+        for (let line of lines) {            
             nes.getCPU().step();
             let dump = nes.getCPU().dumpDebug();           
             line = line.replace(/\s{2,}/g, ' ');
