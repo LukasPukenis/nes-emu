6ade4b98d4ea6a6e595e247d4002f0c1971b5707
Lukas Pukenis <lukas.pukenis@gmail.com>
2019-03-27 22:53:38 +0200

    most basic PPU implementation. Direct rendering(in one go) of nametable at 0x2000

--- src/nes.ts
+++ src/nes.ts
@@ -2,27 +2,31 @@ import { ROM } from './rom';
 import { CPU, CPU_FREQ } from './cpu';
 import { PPU } from './ppu';
 import { Memory } from './memory';
+import { Mapper } from './mappers/mapper';
+import { Mapper0 } from './mappers/mapper0';
+
 
 export class NES {
     private cpu: CPU;
     private ppu: PPU;
     private memory: Memory;
     private rom: ROM;
+    private mapper: Mapper;
     private lastTime: number = 0;
     
     constructor(canvas: HTMLCanvasElement) {
         console.assert(canvas);
-        
-        this.memory = new Memory(this);
 
+        this.rom = new ROM();
+        this.memory = new Memory(this);        
         this.ppu = new PPU(this, canvas && canvas.getContext('2d'));
+        this.cpu = new CPU(this);
 
-        this.cpu = new CPU(this);  
+        this.mapper = new Mapper0(this.rom, this.cpu, this.ppu);
     }
      
     async load(path: string, startingAddress?: number) {
-        console.log('loading...');
-        this.rom = new ROM();
+        console.log('loading...');        
         await this.rom.load(path)        
         this.cpu.loadROM(this.rom, startingAddress);
         console.log('loaded!');
@@ -40,21 +44,21 @@ export class NES {
         return this.memory;
     }
 
+    getMapper(): Mapper {
+        return this.mapper;
+    }
+
     step() {
         let currTime = new Date().getTime();        
         let dt = (currTime - this.lastTime) / 1000.0;
         this.lastTime = currTime;
 
         let cyclesToRun = dt * CPU_FREQ;
-        // console.log('cyclesToRun:', cyclesToRun);
-    
-        while (cyclesToRun > 0) {
-            let cpuCyclesUsed = this.cpu.step();
-            
-            // if (this.startDebug && false)
-            // @ts-ignore
-            // window.debug.push(this.cpu.dumpDebug());
-
+        // console.log('cyclesToRun:', Math.round(cyclesToRun));
+        
+        // @ts-ignore
+        while (cyclesToRun > 0 && !window.finish) {
+            let cpuCyclesUsed = this.cpu.step();                        
             cyclesToRun -= cpuCyclesUsed;
                         
             // ppu runs at exactly 3 times the CPU clock
@@ -62,6 +66,10 @@ export class NES {
                 this.ppu.step();
             }
         }
+
+        // lets render stuff in one go - this should work for most basic NROM games
+        // which do not do any trickeri per scanline, no visual effects based on ppu timing
+        this.ppu.render();
 
     }
 
--- src/ppu.ts
+++ src/ppu.ts
@@ -10,25 +10,44 @@ import { CPU } from "./cpu";
 export class PPU {
     nes: NES;
     vram: Uint8Array;
-    writeToggle: boolean;
+    writeToggle: boolean = false;
     address: number;
-    tempAddrToWrite: number;
+    tempAddrToWrite: number = 0;
     bufferedReadValue: number;
     
     cycle: number = 0;
-    oddFrame: boolean = false;
+    oddFrame: boolean = true;
     scanLine: number = 0;
     frame: number = 0;
 
     nmiDelay: number = 0;
     register: number = 0;
     nmiPrevious: boolean = false;
+
+    spritePatterns: number[] = [];
+    spritePositions: number[] = [];
+    spritePriorities: number[] = [];
+    spriteIndexes: number[] = [];
+
+    flagOverflow: boolean = false;
+    flagZeroHit: boolean = false;
+    
+    spriteCount: number = 0;
+    tileData: number = 0;
+    lowTileByte: number = 0;
+    highTileByte: number = 0;
+    flagBGTable: number = 0;
+    attributeTableByte: number = 0;
+    nameTableByte: number = 0;
+
+    maskShowLeftBG: boolean;
+    maskShowLeftSprites: boolean;
     maskGrayscale: boolean;
-    maskShowBG: boolean;
-    maskShowSprites: boolean;
+    maskShowBG: boolean = false;
+    maskShowSprites: boolean = false;
 
     controlIncrement: boolean;// 0: add 1; 1: add 32
-    controlNameTable: boolean;// 0: $2000; 1: $2400; 2: $2800; 3: $2C00
+    controlNameTable: number;// 0: $2000; 1: $2400; 2: $2800; 3: $2C00
     controlSpriteTable: boolean;// 0: $0000; 1: $1000; ignored in 8x16 mode
     controlBackgroundTable: boolean;// 0: $0000; 1: $1000
     controlSpriteSize: boolean;// 0: 8x8; 1: 8x16
@@ -42,6 +61,7 @@ export class PPU {
     canvas: CanvasRenderingContext2D;
     imageData: ImageData;
     pixels: Uint8ClampedArray;
+    debugInfo: string[] = [];
 
     constructor(nes: NES, canvas: CanvasRenderingContext2D) {    
         this.canvas = canvas;
@@ -51,49 +71,9 @@ export class PPU {
         }
 
         this.nes = nes;
-        this.vram = new Uint8Array(8*1024);
-    }
-
-    copyX() {
-        this.address = (this.address & 0xFBE0) |  | (this.tempAddrToWrite & 0x041F);
-    }
-
-    copyY() {
-
-    }
-
-    incrementX() {
-
-    }
-
-    incrementY() {
-
+        this.vram = new Uint8Array(0x8000);
     }
     
-    storeTile() {
-
-    }
-    
-    fetchNameTableByte() {
-
-    }
-
-    fetchAttributeTableByte() {
-
-    }
-
-    fetchLowTileByte() {
-
-    }
-
-    fetchHighTileByte() {
-
-    }
-
-    renderPixel(): void {
-        throw new Error("NOT YET");
-    }
-
     nmiChange() {
         let nmi = this.generateNMI && this.v_blank;
 
@@ -103,14 +83,25 @@ export class PPU {
         this.nmiPrevious = nmi;
     }
     
-    step() {
+    tick() {
         if (this.nmiDelay > 0) {
             this.nmiDelay--;
             if (this.nmiDelay == 0 && this.v_blank && this.generateNMI) {
+                // console.log('nmi');
                 this.nes.getCPU().triggerNMI();
             }
         }
-        
+
+        if (this.maskShowBG || this.maskShowSprites) {
+            if (this.oddFrame && this.scanLine == 261 && this.cycle == 339) {
+                this.cycle = 0;
+                this.scanLine = 0;
+                this.frame++;
+                this.oddFrame = !this.oddFrame;
+                return;
+            }
+        }
+
         this.cycle++
         if (this.cycle > 340) {
             this.cycle = 0;
@@ -121,8 +112,130 @@ export class PPU {
                 this.oddFrame = !this.oddFrame;
             }
         }
+    }
 
+    render() {
+        // @ts-ignore
+        // if (!window.render) return;
 
+        // 1) nametable byte
+        // 2) data from chrom
+        // 3) pixels
+        // 4) putImageData
+
+        for (let i = 0; i < 256*240*4; i++) {
+            // this.pixels[i] = Math.random()*0xFF;
+        }
+        
+        let d = [];
+
+        for (let j = 0; j < 30; j++) {
+            for (let i = 0; i < 32; i++) {
+                let pixelIdx = (j*256*8 + i*8) * 4 ;                
+                
+                let index = 0x2000 + (j << 5) + i; // todo proper starting address
+                
+                // d.push(index);
+
+                let nametable = this.read(index) + 0x100;
+                
+                // nametable = 0xD3 * 16;
+                // nametable = 0xD1;
+
+                // console.log("====");
+                for (let n = 0; n < 8; n++) {
+                    // if (nametable == 0xD3) debugger;
+                    let line0 = this.read((nametable*16)+n + 0);
+                    let line1 = this.read((nametable*16)+n + 8);
+
+                    for (let m = 0; m < 8; m++) {
+                        let reduct = 7 - m;
+                        let val0 = (line0 >> reduct) & 1;
+                        let val1 = (line1 >> reduct) & 1;
+                        let finalVal = val0 + val1;
+                        
+                        let pixelOffset = (n * 256 + m) * 4;
+                        let finalPixelIdx = pixelIdx + pixelOffset;
+
+                        this.pixels[finalPixelIdx + 0] = finalVal * 50;
+                        this.pixels[finalPixelIdx + 1] = finalVal * 50;
+                        this.pixels[finalPixelIdx + 2] = finalVal * 50;
+                        this.pixels[finalPixelIdx + 3] = 0xff;                        
+                    }                    
+                }                
+            }
+        }
+
+        // console.log(d.length, '===', d);
+        // debugger;
+
+        this.canvas.putImageData(this.imageData, 0, 0);
+
+        // let idx = 0x1D3 * 16;
+        // let chrrom = nes.rom.getCHRROMS()[0];
+        // for (let i = 0; i < 16; i++) {
+        //    let v = chrrom[idx + i] & 0xFF;
+        //    console.log(i.toString().padStart(2, '0'), v.toString(2).padStart(8, '0'));
+        // }
+       
+
+        // function getSprite(index) {
+        //     let idx = index * 16;
+        //     let output1 = [];
+        //     let output2 = [];
+            
+        //     let output = [];
+          
+        //     for (let i = 0; i < 8; i++) {
+        //         let byte1 = nes.ppu.vram[idx+i];  
+        //         let byte2 = nes.ppu.vram[idx+i+8];
+                
+          
+        //         for (let j = 0; j < 8; j++) {
+        //             let bit0 = byte1 & 1;
+        //             let bit1 = byte2 & 1;
+        //             let finalByte = bit0 + bit1;
+        //             output.push(finalByte);
+        //             byte1 >>= 1;
+        //             byte2 >>= 1;
+        //         }
+        //     }
+          
+        //     return output;
+        //   }
+          
+        //   function printSprite(index) {
+        //   for (let i = 0; i < 256*240; i++) {
+        //     let idx = i*4;
+        //     nes.ppu.pixels[idx+0] = 0xFF;
+        //     nes.ppu.pixels[idx+1] = 0x0;
+        //     nes.ppu.pixels[idx+2] = 0x0;
+        //     nes.ppu.pixels[idx+3] = 0xFF;
+        //   }
+          
+          
+        //       let data = getSprite(index);
+              
+        //       for (let i = 0; i < 8; i++) {
+        //           for (let j = 0; j < 8; j++) {
+        //               let pidx = i * 8 + j;
+        //               let idx = (i * 256+ + j) * 4;
+        //               nes.ppu.pixels[idx + 0] = data[pidx] * 50;
+        //               nes.ppu.pixels[idx + 1] = data[pidx] * 50;
+        //               nes.ppu.pixels[idx + 2] = data[pidx] * 50;
+        //               nes.ppu.pixels[idx + 3] = data[pidx] * 0xFF;
+        //           }        
+        //       }
+          
+        //       nes.ppu.canvas.putImageData(nes.ppu.imageData, 0, 0);
+        //   }
+          
+          
+    }
+
+    step() {
+        this.tick();
+        
         //////////////
         /////////////
         let preLine = this.scanLine == 261;
@@ -131,55 +244,18 @@ export class PPU {
         let preFetchCycle = this.cycle >= 321 && this.cycle <= 336;
         let visibleCycle = this.cycle >= 1 && this.cycle <= 256;
         let fetchCycle = preFetchCycle || visibleCycle;
-        let render: boolean = this.maskShowBG || this.maskShowSprites;
+        let render: boolean = this.maskShowBG || this.maskShowSprites;                        
 
-        if (render) {
-            if (visibleCycle && visibleLine) {
-                renderPixel();
-            }     
-
-            if (renderLine && fetchCycle) {
-                let fetchAction = this.cycle % 8;
-                if (fetchAction == 0) {
-                    this.storeTile();
-                } else if (fetchAction == 1) {
-                    this.fetchNameTableByte();
-                } else if (fetchAction == 3) {
-                    this.fetchAttributeTableByte();
-                } else if (fetchAction == 5) {
-                    this.fetchLowTileByte();
-                } else if (fetchAction == 7) {
-                    this.fetchHighTileByte();
-                }
-            }
-
-            if (preLine && this.cycle >= 280 && this.cycle <= 304) {
-                this.copyY();
-            }
-
-            if (renderLine) {
-                if (fetchCycle && this.cycle % 8 == 0) {
-                    this.incrementX();
-                }
-
-                if (this.cycle == 256) {
-                    this.incrementY();
-                }
-
-                if (this.cycle == 257) {
-                    this.copyX();
-                }
-            }            
-        }
-       
-        ////////////
         if (this.scanLine == 241 && this.cycle == 1) {
-            this.setVBlank(true);
+            this.setVerticalBlank();
         }
 
-        if (this.scanLine == 261 && this.cycle == 1) {
-            this.setVBlank(false);
+        if (preLine && this.cycle == 1) {
+            this.clearVerticalBlank();            
+            this.flagZeroHit = false;
+            this.flagOverflow = false;
         }
+               
     }
 
     readStatus(poke: boolean): number {
@@ -197,7 +273,7 @@ export class PPU {
         
         if (!poke) {
             this.v_blank = false;
-            this.writeToggle = true; // todo: check if correct
+            this.writeToggle = false;
         }
 
         this.nmiChange();
@@ -206,6 +282,9 @@ export class PPU {
     }    
 
     write(addr: number, value: number): void {
+        // console.log('PPU write', addr.toString(16), value.toString(2));
+        this.register = value;
+
         if (addr == 0x2000) {
             this.writeControl(value);
         } else if (addr == 0x2001) {
@@ -223,7 +302,15 @@ export class PPU {
         }
     }
 
-    read(addr: number, poke: boolean = false): number {
+    read(addr: number, poke: boolean = false): number {        
+        if (addr < 0x2000) {
+            return this.nes.getMapper().read(addr);
+        } else {
+            return this.vram[addr];
+        }
+    }
+
+    readRegister(addr: number, poke: boolean = false): number {
         if (addr == 0x2000) {
             return this.readControl(addr, poke);
         } else if (addr == 0x2002) {
@@ -243,15 +330,17 @@ export class PPU {
         }
     }
 
-    writeToAddress(value: number): void {
-        this.vram[this.address] = value & 0xFF;
-        this.address += 1; // + 32 if some flag is set
+    writeToAddress(value: number): void {        
+        console.log(this.address.toString(16), '->', value.toString(16));
+        
+        this.vram[this.address] = value & 0xFF;        
+        this.address += this.controlIncrement ? 32 : 1;
     }
 
     readFromAddress(poke: boolean): number {
         let t = this.vram[this.address];
 
-        if (this.address <= 0x3EFF) {
+        if ((this.address % 0x4000) <= 0x3F00) {
             let buf = this.bufferedReadValue;
             this.bufferedReadValue = t;
             t = buf;
@@ -259,34 +348,49 @@ export class PPU {
             this.bufferedReadValue = this.vram[this.address - 0x1000];
         }
 
+        this.address += this.controlIncrement ? 32 : 1;
+
         return t;
     }
 
     writeAddress(value: number): void {
-        value &= 0xFFFF;
+        value &= 0xFF;
 
-        if (this.writeToggle) {
-            this.tempAddrToWrite = (value & 0x3F) << 8;        
+        if (!this.writeToggle) {
+            this.tempAddrToWrite = (this.tempAddrToWrite & 0x80ff) | ((value & 0x3F) << 8);
         } else {
-            this.address = this.tempAddrToWrite | value;
+            this.address = (this.tempAddrToWrite & 0xFF00) | value;
         } 
 
         this.writeToggle = !this.writeToggle;
     }
     
-    writeControl(value: number): void {        
-        this.controlNameTable =   ((value >> 0) & 3) == 1;
+    setVerticalBlank() {
+        this.v_blank = true;
+        this.nmiChange();
+    }
+
+    clearVerticalBlank() {
+        this.v_blank = false;
+        this.nmiChange();
+    }
+
+    writeControl(value: number): void {
+        // console.log('write ctrl', value.toString(2));
+
+        this.controlNameTable =   ((value >> 0) & 3);
         this.controlIncrement =   ((value >> 2) & 1) == 1;
         this.controlSpriteTable = ((value >> 3) & 1) == 1;
         this.controlBackgroundTable = ((value >> 4) & 1) == 1;
         this.controlSpriteSize =      ((value >> 5) & 1) == 1;
-        this.controlMasterSlave =     ((value >> 6) & 1) == 1 
+        this.controlMasterSlave =     ((value >> 6) & 1) == 1         
         this.generateNMI =            ((value >> 7) & 1) == 1;
+        
+        this.nmiChange();
     }
     
     writeMask(value: number): void {
-        console.log('write mask', value.toString(2));
-        debugger
+        // console.log('write mask', value.toString(2));
         this.maskGrayscale = (value & 1) == 1;
         this.maskShowBG = (value >> 3) == 1;
         this.maskShowSprites = (value >> 4) == 1;
@@ -303,12 +407,13 @@ export class PPU {
         this.oamAddress++;    
     }
 
-    writeDMA(value: number): void {
+    writeDMA(value: number): void {        
         let cpu = this.nes.getCPU();
         let memory = this.nes.getMemory();
 
         let addr = value << 8;
         for (let i = 0; i < 256; i++) {
+            let val = memory.read(addr);
             this.oamData[this.oamAddress] = memory.read(addr);
             this.oamAddress++;
             addr++;
